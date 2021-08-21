49419ca6aad3a575560f369b8ef51fa29cab76f3
Ikmal Azman <opikmalazman@gmail.com>
2021-08-21 21:46:07 +0800

    Create bottom card view for itenary details

---
+++ Travl.iOS/View/Card/CardVC.swift
@@ -0,0 +1,31 @@
+//
+//  CardVC.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 21/08/2021.
+//
+
+import UIKit
+
+class CardVC: UIViewController {
+
+    @IBOutlet weak var handlerArea: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
