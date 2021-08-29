684563515cf9ddf76bdb731668ff150e1f239016
Ikmal Azman <opikmalazman@gmail.com>
2021-08-29 20:12:55 +0800

    install new extension for UIView, load nib and drop shadow effects

---
+++ Travl.iOS/Extensions/UIViewExtensions.swift
@@ -0,0 +1,36 @@
+//
+//  UIViewExtensions.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 29/08/2021.
+//
+
+import UIKit.UIView
+
+extension UIView {
+    
+   /// Get nib file name and return to first UIView
+    func loadViewFromNib(nibName : String) -> UIView? {
+        // Get bundle , to find class in which library
+        let bundle = Bundle(for: type(of: self))
+        // Create instance of UINib
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        // Inatiate and get first object from array and cast to UIView
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+  }
+    
+    /// Add drop down shadow to view
+    func dropShadow(scale: Bool = true, radius: CGFloat = 10, opacity : Float = 0.2) {
+           layer.masksToBounds = false
+           layer.shadowColor = UIColor.lightGray.cgColor
+           layer.shadowOpacity = opacity
+           layer.shadowOffset = CGSize(width: -1, height: 1)
+           layer.shadowRadius = radius
+           
+           let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: radius)
+           layer.shadowPath = shadowPath.cgPath
+           layer.shouldRasterize = true
+           layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+       }
+    
+}
