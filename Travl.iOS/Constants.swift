3081db1d990ca23fc352b51d7d89606f833bb104
Ikmal Azman <opikmalazman@gmail.com>
2021-08-31 08:35:29 +0800

    create constants to identify device height, width property

---
+++ Travl.iOS/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 31/08/2021.
+//
+
+import UIKit
+
+struct Constants {
+    struct Device {
+        static var height : CGFloat {
+            return UIScreen.main.bounds.height
+        }
+        static var width : CGFloat {
+            return UIScreen.main.bounds.width
+        }
+    }
+}
