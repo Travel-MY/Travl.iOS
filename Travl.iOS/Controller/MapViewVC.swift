9403d2fcd3e6c0e9436daecbcd89c6d8723d6e9e
Ikmal Azman <opikmalazman@gmail.com>
2021-09-22 09:30:56 +0800

    develop full screen map kit view which trigger from preview map cell

---
+++ Travl.iOS/Controller/MapViewVC.swift
@@ -0,0 +1,62 @@
+//
+//  MapViewVC.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 14/09/2021.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+final class MapViewVC: UIViewController {
+    
+    //MARK:- Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var latitude : Double?
+    var logitude : Double?
+    var locationName : String?
+
+    
+    //MARK:- Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMapView(lat: latitude ?? 0.0, lon: logitude ?? 0.0)
+    }
+    
+    //MARK:- Action
+    @IBAction func undoButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+//MARK:- Private Methods
+extension MapViewVC {
+    
+    private func configureMapView(lat : Double, lon : Double) {
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.0050, longitudeDelta: 0.0050)
+        
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        print(region)
+        
+        mapView.setRegion(region, animated: true)
+        
+        configureAnnotation(from : coordinate)
+        
+        
+    }
+    
+    private func configureAnnotation( from coordinate : CLLocationCoordinate2D) {
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = locationName
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        
+    }
+}
