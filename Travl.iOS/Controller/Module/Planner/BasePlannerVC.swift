fefec5d44dd2080b773cd38bed505e18d72ab60c
Ikmal Azman <opikmalazman@gmail.com>
2021-09-24 19:47:20 +0800

    initialise base vc for planner features

---
+++ Travl.iOS/Controller/Module/Planner/BasePlannerVC.swift
@@ -0,0 +1,29 @@
+//
+//  BasePlannerVC.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 24/09/2021.
+//
+
+import UIKit
+
+class BasePlannerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
