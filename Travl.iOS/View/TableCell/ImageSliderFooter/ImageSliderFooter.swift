0153c79403321184c11f1850626c4b1dbfd91e04
Ikmal Azman <opikmalazman@gmail.com>
2021-10-02 06:44:33 +0800

    create custom image slider for tableview footer with page control indicator and stubs images

---
+++ Travl.iOS/View/TableCell/ImageSliderFooter/ImageSliderFooter.swift
@@ -0,0 +1,38 @@
+//
+//  ImageSliderFooter.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 01/10/2021.
+//
+
+import UIKit
+
+final class ImageSliderFooter: UITableViewHeaderFooterView {
+
+    //MARK:- Variables
+    @IBOutlet weak var sliderScollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    //MARK:- LifeCycles
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        sliderScollView.delegate = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: 20))
+        contentView.layer.cornerRadius = 10
+    }
+}
+
+//MARK:- UIScrollView Delegate
+extension ImageSliderFooter : UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let pageIndex = Int(sliderScollView.contentOffset.x / Constants.Device.width)
+        pageControl.currentPage = pageIndex
+        
+    }
+}
