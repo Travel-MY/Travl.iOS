aa8ddd8e3dfa6da8f40299850ffe72a3365ee5eb
Ikmal Azman <opikmalazman@gmail.com>
2021-10-02 06:42:52 +0800

    assign custom footer and header for tableview, base setup for planner tab home page

--- Travl.iOS/Controller/Module/Planner/BasePlannerVC.swift
+++ Travl.iOS/Controller/Module/Planner/BasePlannerVC.swift
@@ -9,21 +9,62 @@ import UIKit
 
 class BasePlannerVC: UIViewController {
 
+    //MARK:- Outlets
+    @IBOutlet weak var basePlannerTableView: UITableView!
+    
+    //MARK:- Variables
+    var plannerData : Any = []
+    
+    //MARK:- LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        renderView()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK:- TV DataSource
+extension BasePlannerVC : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
 
+//MARK:- TV Delegate
+extension BasePlannerVC : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}
+
+//MARK:- CreateATripHeader Delegate
+extension BasePlannerVC : CreateATripHeaderDelegate {
+    func didTapTripButton(view: Any) {
+        print("Click")
+        performSegue(withIdentifier: "goToCreatePlanner", sender: self)
+    }
+}
+
+//MARK:- Private methods
+extension BasePlannerVC {
+    
+    private func renderView() {
+        
+        basePlannerTableView.delegate = self
+        basePlannerTableView.dataSource = self
+        
+        let header = Bundle.main.loadNibNamed(R.nib.createATripHeader.identifier, owner: nil, options: nil)?.first as! CreateATripHeader
+        basePlannerTableView.tableHeaderView = header
+        
+        header.setViewDelegate(delegate: self)
+        
+        let footer = Bundle.main.loadNibNamed(R.nib.imageSliderFooter.identifier, owner: nil, options: nil)?.first as! ImageSliderFooter
+        
+        basePlannerTableView.tableFooterView = footer
+
+    }
 }
