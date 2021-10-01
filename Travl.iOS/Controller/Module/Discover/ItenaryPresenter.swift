3a72ed239cd13581586b7eb6bff95d9637da36ff
Ikmal Azman <opikmalazman@gmail.com>
2021-10-02 06:41:31 +0800

    refactor network call logic from itenaryVC to presenter

---
+++ Travl.iOS/Controller/Module/Discover/ItenaryPresenter.swift
@@ -0,0 +1,35 @@
+//
+//  ItenaryPresenter.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 02/10/2021.
+//
+
+import Foundation
+
+protocol ItenaryPresenterDelegate : AnyObject {
+    func presentItenaryData(with data : [[Days]])
+}
+
+class ItenaryPresenter {
+    
+    weak var delegate : ItenaryPresenterDelegate?
+    
+    func setViewDelegate(delegate : ItenaryPresenterDelegate) {
+        self.delegate = delegate
+    }
+    
+    func getItenaries(for location : String) {
+        NetworkManager.shared.getItenaries(for: location) { [weak self] data in
+            
+            switch data {
+                
+            case .success(let itenaries):
+                self?.delegate?.presentItenaryData(with: itenaries)
+                
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
+    }
+}
