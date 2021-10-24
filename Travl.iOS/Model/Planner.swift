e1c48d640e4129c8c1a931611f8453980f4e6fae
Ikmal Azman <opikmalazman@gmail.com>
2021-10-24 20:56:57 +0800

    adding planner data models

---
+++ Travl.iOS/Model/Planner.swift
@@ -0,0 +1,14 @@
+//
+//  Planner.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 24/10/2021.
+//
+
+import Foundation
+
+struct Planner {
+    let destination : String
+    let startDate : String
+    let endDate : String
+}
