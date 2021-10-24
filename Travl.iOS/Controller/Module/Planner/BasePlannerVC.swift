7c8cfe577d672b603a58fb18ae664bb9a47b850e
Ikmal Azman <opikmalazman@gmail.com>
2021-10-24 20:56:40 +0800

    adding methods to receive created planner by user from createPlannerVC to basePlannerVC

--- Travl.iOS/Controller/Module/Planner/BasePlannerVC.swift
+++ Travl.iOS/Controller/Module/Planner/BasePlannerVC.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class BasePlannerVC: UIViewController {
-
+    
     //MARK:- Outlets
     @IBOutlet weak var basePlannerTableView: UITableView!
     
     //MARK:- Variables
-    var plannerData : Any = []
+    var plannerData = [Planner]()
     
     //MARK:- LifeCycle
     override func viewDidLoad() {
@@ -21,30 +21,45 @@ class BasePlannerVC: UIViewController {
         renderView()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == R.segue.basePlannerVC.goToCreatePlanner.identifier {
+            let createPlannerVC = segue.destination as! CreatePlannerVC
+            createPlannerVC.delegate = self
+        }
+    }
 }
 
 //MARK:- TV DataSource
 extension BasePlannerVC : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return plannerData.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let listOfPlanner = plannerData[indexPath.row]
+        
+        let cell = basePlannerTableView.dequeueReusableCell(withIdentifier: R.nib.plannerItemsCell.identifier, for: indexPath) as! PlannerItemsCell
+        cell.configureCell(data: listOfPlanner)
+        return cell
     }
 }
 
 //MARK:- TV Delegate
 extension BasePlannerVC : UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        basePlannerTableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
 //MARK:- CreateATripHeader Delegate
 extension BasePlannerVC : CreateATripHeaderDelegate {
     func didTapTripButton(view: Any) {
-        print("Click")
         performSegue(withIdentifier: "goToCreatePlanner", sender: self)
     }
 }
@@ -53,9 +68,17 @@ extension BasePlannerVC : CreateATripHeaderDelegate {
 extension BasePlannerVC {
     
     private func renderView() {
-        
         basePlannerTableView.delegate = self
         basePlannerTableView.dataSource = self
+        
+        basePlannerTableView.estimatedRowHeight = 110
+        basePlannerTableView.rowHeight = UITableView.automaticDimension
+        
+        registerCustomNib()
+    }
+    
+    private func registerCustomNib() {
+        basePlannerTableView.register(PlannerItemsCell.nib(), forCellReuseIdentifier: R.reuseIdentifier.plannerItemsCell.identifier)
         
         let header = Bundle.main.loadNibNamed(R.nib.createATripHeader.identifier, owner: nil, options: nil)?.first as! CreateATripHeader
         basePlannerTableView.tableHeaderView = header
@@ -65,6 +88,18 @@ extension BasePlannerVC {
         let footer = Bundle.main.loadNibNamed(R.nib.imageSliderFooter.identifier, owner: nil, options: nil)?.first as! ImageSliderFooter
         
         basePlannerTableView.tableFooterView = footer
+        
+     
+    }
+}
 
+//MARK:- CreatePlannerDelegate
+extension BasePlannerVC : CreatePlannerDelegate {
+    
+    func didCreatePlanner(_ CreatePlanner: CreatePlannerVC, data: Planner) {
+    
+        plannerData.append(data)
+        basePlannerTableView.reloadData()
+        print(plannerData.count)
     }
 }
