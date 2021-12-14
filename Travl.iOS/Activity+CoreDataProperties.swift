ff253a90cd1e331e2eb7faf59a671d75c5867625
Ikmal Azman <opikmalazman@gmail.com>
2021-12-14 22:24:56 +0800

    create data model for activity and its relationship to planner

---
+++ Travl.iOS/Activity+CoreDataProperties.swift
@@ -0,0 +1,32 @@
+//
+//  Activity+CoreDataProperties.swift
+//  Travl.iOS
+//
+//  Created by Ikmal Azman on 12/12/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Activity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Activity> {
+        return NSFetchRequest<Activity>(entityName: "Activity")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var address: String?
+    @NSManaged public var startDate: String?
+    @NSManaged public var endDate: String?
+    @NSManaged public var phoneNumber: String?
+    @NSManaged public var website: String?
+    @NSManaged public var notes: String?
+    @NSManaged public var parentPlanner: Planner
+
+}
+
+extension Activity : Identifiable {
+
+}
