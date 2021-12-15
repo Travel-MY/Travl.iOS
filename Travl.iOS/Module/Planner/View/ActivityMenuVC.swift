c72e4b84cc50dbf73a3f77af5759d8b91289e8a5
Ikmal Azman <opikmalazman@gmail.com>
2021-12-15 15:28:40 +0800

    move presentation and handling logic to activity menu presenter

--- Travl.iOS/Module/Planner/View/ActivityMenuVC.swift
+++ Travl.iOS/Module/Planner/View/ActivityMenuVC.swift
@@ -8,32 +8,39 @@
 import UIKit
 
 final class ActivityMenuVC: UIViewController {
-
     //MARK: - Outlets
     @IBOutlet weak var activityCollectionView: UICollectionView!
-    
     //MARK: - Variables
-    var menuItem = [Menu]()
-    var selectedRow = 0
     var data : Planner?
-
+    
+    private let presenter = ActivityMenuPresenter()
+    private var selectedRow = 0
+    private var menuItem = [Menu]()
     private var newActivity : Activity? {
         didSet {
             observerActions()
         }
     }
-    
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         renderView()
-        NotificationCenter.default.addObserver(self, selector: #selector(observerActions), name: Notification.Name(rawValue: "Dismiss"), object: nil)
+        presenter.setViewDelegate(delegate: self)
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tourMenuVC = segue.destination as? TourMenuVC {
+            if let destination = UserDefaults.standard.string(forKey: Constants.UserDefautlsKey.primaryKeyForPlanner) {
+                tourMenuVC.destinationName = destination
+            }
+            let selectedActivity = menuItem[selectedRow]
+            tourMenuVC.navBarLabel = selectedActivity.label
+        }
     }
     
     @objc func observerActions() {
         navigationController?.popViewController(animated: true)
     }
-
 }
 
 //MARK: - CV Datasource
@@ -48,36 +55,20 @@ extension ActivityMenuVC : UICollectionViewDataSource {
         cell.setCell(data: listOfMenu)
         return cell
     }
-    
 }
 //MARK: - CV Delegate
 extension ActivityMenuVC : UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         debugPrint("selected activity at row : \(indexPath.row)")
-        switch indexPath.row {
-        case 0 :
-            selectedRow = indexPath.row
-            performSegue(withIdentifier: "goToTourMenu", sender: self)
-        case 1 :
-            selectedRow = indexPath.row
-            performSegue(withIdentifier: "goToTourMenu", sender: self)
-        case 2 :
-            return
-        case 3 :
-            return
-        default :
-            return
-        }
+        presenter.didTapActivityMenu(atIndex: indexPath.row)
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToTourMenu" {
-            let selectedActivity = menuItem[selectedRow]
-            let destinationVC = segue.destination as! TourMenuVC
-            if let destination = UserDefaults.standard.string(forKey: "parentPlanner") {
-                destinationVC.destinationName = destination
-            }
-            destinationVC.navBarLabel = selectedActivity.label
+}
+//MARK: - Presenter Delegate
+extension ActivityMenuVC : ActivityMenuPresenterDelegate {
+    func presentToTourMenu(_ ActivityMenuPresenter: ActivityMenuPresenter, index: Int) {
+        selectedRow = index
+        if index <= 1 {
+            performSegue(withIdentifier: Constants.SegueIdentifier.goToTourMenu, sender: self)
         }
     }
 }
@@ -91,9 +82,9 @@ extension ActivityMenuVC {
         menuItem =  [
             Menu(image: "map.circle.fill", label: "Location"),
             Menu(image: "fork.knife.circle.fill", label: "Restaurant"),
-//            Menu(image: "bed.double.circle.fill", label: "Lodging"),
-//            Menu(image: "airplane.circle.fill", label: "Flight")
+            //            Menu(image: "bed.double.circle.fill", label: "Lodging"),
+            //            Menu(image: "airplane.circle.fill", label: "Flight")
         ]
+        NotificationCenter.default.addObserver(self, selector: #selector(observerActions), name: Notification.Name(rawValue: "Dismiss"), object: nil)
     }
-    
 }
